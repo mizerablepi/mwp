995bdcf85a53229f23bedbb302e3238799ce130d
miz <mizerablepi@gmail.com>
2023-02-23 20:55:19 +0530

    complete till api call

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def home():
-    return render_template('app.html')
+    data = request.form.get('input','')
+    return render_template('app.html',output=data)
+
+# @app.route('/api',methods=['GET','POST'])
+# def api():
+#     return request.form['input']
