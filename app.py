b063592632f4ce7ecd1b25f2daf05a36376ce1d6
miz <mizerablepi@gmail.com>
2023-02-24 16:14:57 +0530

    made preprocessing page and methods

--- app.py
+++ app.py
@@ -1,37 +1,40 @@
 import os
 from flask import Flask,render_template,request
-import openai
+from modules import test
+#import openai
 
-my_secret = os.environ['KEY']
-openai.api_key = my_secret
-
-def generate_text(prompt):
-    completions = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt+"show with an equation",
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.0,
-        top_p=1.0,
-        presence_penalty=-0.6,
-    )
-
-    message = completions.choices[0].text
-    return message.strip()
+# def generate_text(prompt):
+    # my_secret = os.environ['KEY']
+    # openai.api_key = my_secret
+    # completions = openai.Completion.create(
+        # engine="text-davinci-003",
+        # prompt=prompt+" show with an equation",
+        # max_tokens=1024,
+        # n=1,
+        # stop=None,
+        # temperature=0.0,
+        # top_p=1.0,
+        # presence_penalty=-0.6,
+    # )
+# 
+    # message = completions.choices[0].text
+    # return message.strip()
   
 app = Flask(__name__)
-
+data = ''
 @app.route('/', methods=['GET','POST'])
 def home():
     output = ''
-    data = request.form.get('input',None)
-    if data != None:
-      output = generate_text(data)
-    return render_template('app.html',output=output)
+    global data 
+    data = request.form.get('input','')
+    #if data != None:
+      #output = generate_text(data)
+    return render_template('app.html',output=data)
 
-# @app.route('/api',methods=['GET','POST'])
-# def api():
-#     return request.form['input']
+@app.route('/api',methods=['GET','POST'])
+def api():
+    new = test(data)
+    return render_template('api.html',out1=new,out2=data+'second',out3=data+'third') 
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0',debug=True)
